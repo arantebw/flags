382e47c39379f0838baa8177478ee011c648c8b6
Billy Arante <arantebillywilson@gmail.com>
2024-01-17 13:51:19 +0800

    feat: ✨ create the Denmark flag

---
+++ src/denmark.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type Props = {
+  className?: string;
+};
+
+const Denmark: React.FC<Props> = ({ className }: Props) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    id="flag-icons-dk"
+    viewBox="0 0 640 480"
+    width="21"
+    height="14"
+  >
+    <path fill="#c8102e" d="M0 0h640.1v480H0z" />
+    <path fill="#fff" d="M205.7 0h68.6v480h-68.6z" />
+    <path fill="#fff" d="M0 205.7h640.1v68.6H0z" />
+  </svg>
+);
+
+export default Denmark;
