1cbbfabf1c1f6ba85a1ea224e06e974727531900
Billy Arante <arantebillywilson@gmail.com>
2024-01-17 13:36:57 +0800

    feat: ✨ create the Austria flag

---
+++ austria.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Props = {
+  className?: string;
+};
+
+const Austria: React.FC<Props> = ({ className }: Props) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    id="flag-icons-at"
+    viewBox="0 0 640 480"
+    width="21"
+    height="14"
+  >
+    <g fill-rule="evenodd">
+      <path fill="#fff" d="M640 480H0V0h640z" />
+      <path fill="#c8102e" d="M640 480H0V320h640zm0-319.9H0V.1h640z" />
+    </g>
+  </svg>
+);
+
+export default Austria;
